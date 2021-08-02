18b82848b0dbf075e7fc416afadf77280f9ba9f5
Junaed Abed <junaedccpc17@gmail.com>
2021-08-02 16:54:20 +0600

    unban function added

--- bot.py
+++ bot.py
@@ -63,5 +63,19 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
 	await member.ban(reason=reason)
+	await ctx.send(f'Banned {member.mention}')
+	
+@client.command()
+async def unban(ctx, *, member):
+	banner_users = await ctx.guild.bans()
+	member_name, member_discriminator = member.split('#')
+	
+	for ban_entry in banner_users:
+		user = ban_entry.user
+		
+		if(user.name, user.discriminator) == (member_name, member_discriminator):
+			await ctx.guild.unban(user)
+			await ctx.send(f'Unbanned {user.mention}')
+			return
 	
 client.run(os.environ.get('BOT_TOKEN'))
